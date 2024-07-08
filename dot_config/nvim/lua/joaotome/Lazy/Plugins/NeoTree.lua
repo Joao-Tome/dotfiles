a94779fa4b8a7a584ae76e352f02f7cd4e205dbc
Joao-Tome <73352040+Joao-Tome@users.noreply.github.com>
2024-07-08 19:39:48 -0300

    Arrumado neotree

--- dot_config/nvim/lua/joaotome/Lazy/Plugins/NeoTree.lua
+++ dot_config/nvim/lua/joaotome/Lazy/Plugins/NeoTree.lua
@@ -14,8 +14,8 @@ return {
 			enable_git_status = true,
 			enable_diagnostics = true,
 			open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-			sort_case_insensitive = false,                             -- used when sorting files and directories in the tree
-			sort_function = nil,                                       -- use a custom function for sorting files and directories in the tree
+			sort_case_insensitive = false,                            -- used when sorting files and directories in the tree
+			sort_function = nil,                                      -- use a custom function for sorting files and directories in the tree
 			-- sort_function = function (a,b)
 			--       if a.type == b.type then
 			--           return a.path > b.path
@@ -186,11 +186,11 @@ return {
 					},
 				},
 				follow_current_file = {
-					enabled = false,         -- This will find and focus the file in the active buffer every time
+					enabled = false,        -- This will find and focus the file in the active buffer every time
 					--               -- the current file is changed while the tree is open.
 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
 				},
-				group_empty_dirs = false,    -- when true, empty folders will be grouped together
+				group_empty_dirs = false,   -- when true, empty folders will be grouped together
 				hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens neo-tree
 				-- in whatever position is specified in window.position
 				-- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -277,7 +277,8 @@ return {
 				}
 			}
 		},
-		config = function()
+		config = function(_, opts)
+			require("neo-tree").setup(opts)
 			-- If you want icons for diagnostic errors, you'll need to define them somewhere:
 			vim.fn.sign_define("DiagnosticSignError",
 				{ text = " ", texthl = "DiagnosticSignError" })
@@ -289,6 +290,6 @@ return {
 				{ text = "󰌵", texthl = "DiagnosticSignHint" })
 			require('transparent').clear_prefix('NeoTree')
 			vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
-		end
+		end,
 	},
 }
