d6da6b566628152e0e1ba85ed3c3468fc5b34026
João Pedro Tomé <73352040+Joao-Tome@users.noreply.github.com>
2025-07-30 13:55:57 -0300

    Atualizado oh-my-posh do Windows

--- OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ OneDrive/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 Import-Module -name Terminal-Icons
-oh-my-posh init pwsh --config 'C:\Users\joaop\AppData\Local\Programs\oh-my-posh\themes/velvet.omp.json'| Invoke-Expression
+$THEME = "$HOME\.config\oh-my-posh\theme\velvet.omp.json"
+oh-my-posh init pwsh --config $THEME| Invoke-Expression
 clear
 fastfetch
